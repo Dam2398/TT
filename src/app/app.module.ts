5529a0fd10faba98e349012511ebc101e0224fcf
Damian Osornio <ojuandamian@gmail.com>
2021-06-13 22:39:27 -0500

    auxiliar final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { ChartsModule } from "ng2-charts";
 import { KanbanComponent } from './components/kanban/kanban.component';
 import { RegisterComponent } from './components/register/register.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
